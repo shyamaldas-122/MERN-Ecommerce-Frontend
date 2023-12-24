c7ef623ab2b8122d9187125cd69fb8cf6a2fed4e
Shyamal Das <shyamald934@gmail.com>
2023-12-24 11:41:35 +0530

    Setup Redux of Products/ JSON-server/ Products Filters!

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import productReducer from '../features/product/productSlice';
 
 const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productReducer,
   },
 });
 
