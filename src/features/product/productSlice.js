c39ed87c5acd352a08e4da08875e34bc5f0d2a5b
Shyamal Das <shyamald934@gmail.com>
2023-12-24 19:00:47 +0530

    Pagination Added!

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -4,6 +4,7 @@ import { fetchAllProducts,fetchProductsByFilters } from './productAPI';
 const initialState = {
   products: [],
   status: 'idle',
+  totalItems:0
 };
 
 
@@ -17,8 +18,8 @@ export const fetchAllProductsAsync = createAsyncThunk(
 );
 export const fetchProductsByFiltersAsync = createAsyncThunk(
   'product/fetchProductsByFilters',
-  async ({filter,sort}) => {
-    const response = await fetchProductsByFilters(filter,sort);
+  async ({filter,sort,pagination}) => {
+    const response = await fetchProductsByFilters(filter,sort,pagination);
     // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
@@ -34,27 +35,29 @@ export const productSlice = createSlice({
       state.value += 1;
     },
   },
-    extraReducers: (builder) => {
-      builder
-        .addCase(fetchAllProductsAsync.pending, (state) => {
-          state.status = 'loading';
-        })
-        .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
-          state.status = 'idle';
-          state.products = action.payload;
-        })
-        .addCase(fetchProductsByFiltersAsync.pending, (state) => {
-          state.status = 'loading';
-        })
-        .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
-          state.status = 'idle';
-          state.products = action.payload;
-        });
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchAllProductsAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllProductsAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload;
+      })
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
+        state.status = 'idle';
+        state.products = action.payload.products;
+        state.totalItems = action.payload.totalItems;
+      });
+  },
 });
 
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
