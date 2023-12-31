fa35cec7f865a6b58ed74d708dd54783ada009f4
Shyamal Das <shyamald934@gmail.com>
2023-12-31 21:04:17 +0530

    Email and Reset password flow ( backend part 10) !

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ import AdminOrdersPage from './pages/AdminOrdersPage';
 import { positions, Provider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
 import StripeCheckout from './pages/StripeCheckout';
+import ResetPasswordPage from './pages/ResetPasswordPage';
 
 const options = {
   timeout: 5000,
@@ -157,6 +158,10 @@ const router = createBrowserRouter([
   {
     path: '/forgot-password',
     element: <ForgotPasswordPage></ForgotPasswordPage>,
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPasswordPage></ResetPasswordPage>,
   },
   {
     path: '*',
