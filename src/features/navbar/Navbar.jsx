f68d10cde2ef3aa8661643620131c110e461620b
Shyamal Das <shyamald934@gmail.com>
2024-01-01 18:40:29 +0530

    Logout/Timestamps/OrderEmail/ProductDetails Added(Backend Part 11)!

--- src/features/navbar/Navbar.jsx
+++ src/features/navbar/Navbar.jsx
@@ -38,14 +38,14 @@ function NavBar({ children }) {
         <Disclosure as="nav" className="bg-gray-800">
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
                       <Link to="/">
                         <img
                           className="h-8 w-8"
-                          src="./ecommerce.png"
+                          src="/ecommerce.png"
                           alt="Your Company"
                         />
                       </Link>
